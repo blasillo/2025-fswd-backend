c0b834ada397f031ba66c88dabbd2dbcf0641e9e
Blas <blasillous@gmail.com>
2025-03-17 09:25:06 +0100

    lab 6 solucion

---
+++ src/main/java/es/jcyl/formacion/backend/controladores/GestorExcepcion.java
@@ -0,0 +1,53 @@
+package es.jcyl.formacion.backend.controladores;
+
+import es.jcyl.formacion.backend.excepciones.RespuestaExcepcion;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+
+@RestControllerAdvice
+public class GestorExcepcion {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RespuestaExcepcion> gestorNoValido (MethodArgumentNotValidException exp) {
+
+        Set<String> errores = new HashSet<>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var errorMessage = error.getDefaultMessage();
+                    errores.add(errorMessage);
+                });
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(
+                        RespuestaExcepcion.builder()
+                                .erroresValidation( errores )
+                                .build()
+                );
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RespuestaExcepcion> gestorExcepcion (Exception exp) {
+        exp.printStackTrace();
+
+        return  ResponseEntity.status( INTERNAL_SERVER_ERROR )
+                .body(
+                        RespuestaExcepcion.builder()
+                                .codigoError(404)
+                                .descripcionError( "Error en la aplicacion" )
+                                .error( exp.getMessage() )
+                                .build()
+                );
+    }
+}
