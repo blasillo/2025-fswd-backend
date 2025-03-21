cd8ed56fc748ec378a7fba224a61861c11dd5e93
Blas <blasillous@gmail.com>
2025-03-21 13:58:11 +0100

    lab 8 - keycloak -sol

--- src/main/java/es/jcyl/formacion/backend/config/SeguridadConfig.java
+++ src/main/java/es/jcyl/formacion/backend/config/SeguridadConfig.java
@@ -1,30 +1,25 @@
 package es.jcyl.formacion.backend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class SeguridadConfig {
 
-    /*
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Desactiva CSRF
-            .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // Configura frameOptions
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/**").permitAll() // Permite acceso a todos los endpoints
-            );
-
-        return http.build();
-    }
-    */
-
+    public static final String ADMIN = "ADMINISTRADOR";
+    public static final String USER = "BASE";
+    private final JwtConverter jwtConverter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -34,16 +29,19 @@ public class SeguridadConfig {
             .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())) // Configura frameOptions
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/v1/actuator/**","/actuator/**").permitAll()
-                    .requestMatchers("/api/v1/**","/tareas/**").permitAll()
+                    .requestMatchers("/api/v1/**","/tareas/**").hasAnyRole(ADMIN,USER)
                     .anyRequest().authenticated() // Todo lo demás requiere autenticación
             )
             .formLogin(AbstractAuthenticationFilterConfigurer::permitAll )// Permite acceso al formulario de login para todos
             .httpBasic(httpBasic -> {}); // Habilita autenticación HTTP Basic
 
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS));
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
         return http.build();
 
     }
-
+/*
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
@@ -53,6 +51,9 @@ public class SeguridadConfig {
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
+
+
+ */
 
 
 
